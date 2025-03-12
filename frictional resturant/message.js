ed24fde842eda16ca22353c43c0a5a54ee616a91
princepraise-git <princepraise660@outlook.com>
2025-03-12 06:12:32 -0700

    Add files via upload

---
+++ frictional resturant/message.js
@@ -0,0 +1,11 @@
+<script>
+// Get references to the button and the message paragraph
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+
+// Add a click event listener to the button
+button.addEventListener("click", function () {
+    // Display the message
+    message.style.display = "block"
+});
+</script>
